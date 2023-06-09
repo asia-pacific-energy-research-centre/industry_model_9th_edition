b5a38777a60d9f99d23386a27116bc9df894818a
mathewhorne <57614046+mathewhorne@users.noreply.github.com>
2023-06-09 17:21:59 +0900

    Update c3_sc_consolidation.py

--- workflow/scripts/c3_sc_consolidation.py
+++ workflow/scripts/c3_sc_consolidation.py
@@ -1,4 +1,4 @@
-# Cement projections using Machine Learning
+# Consolidated grab of steel and cement baseline production estimates
 
 # Set working directory to be the project folder
 import os
@@ -24,6 +24,53 @@ for economy in APEC_economies:
     for i in filenames:
         temp_df = pd.read_csv(i)
         temp_df['economy_code'] = economy
+        temp_df['production'] = 'Steel production'
+        temp_df['units'] = 'Thousand tonnes' #WSA source
         combined_df = pd.concat([combined_df, temp_df]).copy()
 
 combined_df.to_csv('./data/ml_steel/ml_steel_all.csv', index = False)
+
+# Choice of appropriate ML models for each of the economies
+steel_model_dict = pd.read_csv('./data/config/ml_baseline_steel.csv', index_col = 0).squeeze().to_dict()
+
+interim_steel_df = pd.DataFrame()
+
+for economy in APEC_economies:
+    temp_steel = combined_df[(combined_df['economy_code'] == economy) &
+                             (combined_df['model'].isin(['Historic steel production', 
+                                                         steel_model_dict[economy]]))]\
+                                                            .copy().reset_index(drop = True)
+    
+    interim_steel_df = pd.concat([interim_steel_df, temp_steel]).copy().reset_index(drop = True)
+
+# Save steel df with selected baseline model results for production 
+interim_steel_df.to_csv('./data/ml_steel/ml_steel_selected.csv', index = False)
+
+# Read in ML results: cement
+combined_df = pd.DataFrame()
+
+for economy in APEC_economies:
+    filenames = glob.glob('./data/ml_cement/{}/ml_build/model_predictions*.csv'.format(economy))
+    for i in filenames:
+        temp_df = pd.read_csv(i)
+        temp_df['economy_code'] = economy
+        temp_df['production'] = 'Cement production'
+        temp_df['units'] = 'Thousand tonnes' #USGS source
+        combined_df = pd.concat([combined_df, temp_df]).copy()
+
+combined_df.to_csv('./data/ml_cement/ml_cement_all.csv', index = False)
+
+# Choice of appropriate ML models for each of the economies
+cement_model_dict = pd.read_csv('./data/config/ml_baseline_cement.csv', index_col = 0).squeeze().to_dict()
+
+interim_cement_df = pd.DataFrame()
+
+for economy in APEC_economies:
+    temp_cement = combined_df[(combined_df['economy_code'] == economy) &
+                             (combined_df['model'].isin(['Historic cement production', 
+                                                         cement_model_dict[economy]]))]\
+                                                            .copy().reset_index(drop = True)
+    
+    interim_cement_df = pd.concat([interim_cement_df, temp_cement]).copy().reset_index(drop = True)
+
+interim_cement_df.to_csv('./data/ml_cement/ml_cement_selected.csv', index = False)
